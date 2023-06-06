d4c85cf1297cbea3c08cd2aeaa8f1589fd898b61
hebert santos <hebertsantos0704@gmail.com>
2023-06-06 10:20:11 -0300

    update

--- src/Components/PersonDetail/index.jsx
+++ src/Components/PersonDetail/index.jsx
@@ -1,29 +1,23 @@
 import React from 'react';
-import { Link, useParams } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 import { apiKey } from '../../api';
 import { ContainerDetails, ImageDetails, Overview, MainContainer } from './style';
-import { ModalLanguage } from '../ModalLangauge';
-
 
 export const DetailsPerson = () => {
   const [ personId, setPersonId ] = useState([]);
-  const [ movieLanguage, setMovieLanguage ] = useState(false);
 
   const imageUrl = 'https://image.tmdb.org/t/p/w500/';
   const { id } = useParams();
-  
+
   useEffect(() => {
     axios.get(`https://api.themoviedb.org/3/person/${id}?api_key=${apiKey}&language=en-US&page=4`)
       .then(response => setPersonId(response.data))
       .catch(err => err);
   }, [ id ]);
   
-  function handleClickOpenMovieLanguage(){
-    setMovieLanguage(!movieLanguage);
-  }
-  
+
   return (
     <MainContainer>
       <ContainerDetails>
@@ -32,11 +26,9 @@ export const DetailsPerson = () => {
       </ContainerDetails>
       <Overview>
         <h3>{personId.name}</h3>
-        <Link to='#' onClick={handleClickOpenMovieLanguage}>see name in another language</Link>
         <p>{personId.biography}</p>
         <p>Local nascimento: {personId.place_of_birth}</p>
         <p>depatarmento: {personId.known_for_department}</p>
-        {movieLanguage && <ModalLanguage data={personId.also_known_as}/>}
       </Overview>
     </MainContainer>
   );
--- src/layout/index.jsx
+++ src/layout/index.jsx
@@ -2,15 +2,13 @@ import React from 'react';
 import { Outlet } from 'react-router-dom';
 import { Header } from '../Components/Header';
 import { LayoutStyle } from './style';
-import { FooterContainer } from '../Components/Footer';
-
 
 export const Layout = () => {
   return(
     <LayoutStyle>
       <Header />
       <Outlet />
-      <FooterContainer />
+
     </LayoutStyle>
   );
 };
